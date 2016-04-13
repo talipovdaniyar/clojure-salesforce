1f03d82d831cc5ccb83ccef591d427aa98dd1552
Daniyar <talipov.daniyar@gmail.com>
2016-04-13 13:54:09 +0200

    Changed description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clojure-salesforce "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Testing clojure in combination with Salesforce and Heroku Connect"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
